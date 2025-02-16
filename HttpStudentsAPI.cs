b285784141abbb8f83a654004f32f7aa2fada119
Gary Yu <hyu100@my.bcit.ca>
2025-02-15 21:25:32 -0800

    add getstudents API route

--- HttpStudentsAPI.cs
+++ HttpStudentsAPI.cs
@@ -1,16 +1,21 @@
+using System.Net;
+using FifaGames.Models.School;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace School.Function;
     public class HttpStudentsAPI
     {
       private readonly ILogger<HttpStudentsAPI> _logger;
-
-      public HttpStudentsAPI(ILogger<HttpStudentsAPI> logger)
+      private readonly SchoolContext _context;
+      public HttpStudentsAPI(ILogger<HttpStudentsAPI> logger, SchoolContext context)
       {
-          _logger = logger;
+        _logger = logger;
+        _context = context;
       }
 
       [Function("Welcome")]
@@ -19,5 +24,20 @@ namespace School.Function;
           //_logger.LogInformation("C# HTTP trigger function processed a request.");
           return new OkObjectResult("Welcome my friend!");
       }
+
+      [Function("GetStudents")]
+      //route suggests our endpoint is /api/students
+      public HttpResponseData GetStudents([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "students")] HttpRequestData req)
+      {
+        _logger.LogInformation("C# HTTP GET/posts trigger function processed a request in GetStudents().");
+        var students = _context.Students.ToArray();
+        var response = req.CreateResponse(HttpStatusCode.OK);
+
+        response.Headers.Add("Content-Type", "application/json");
+        response.WriteStringAsync(JsonConvert.SerializeObject(students));
+        
+        return response;
+      }
+
   }
 
---
+++ Models/School/SchoolContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FifaGames.Models.School;
+
+public partial class SchoolContext : DbContext
+{
+    public SchoolContext()
+    {
+    }
+
+    public SchoolContext(DbContextOptions<SchoolContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Students__32C52B99C517F73F");
+
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.School)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
